774fbf7aa3f5064f40a0d98b0544b80f17e7fc1c
tarunkarthick <tarunkarthick.7@gmail.com>
2020-05-19 13:42:20 +0530

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('react/build')); // serve the static react app
-    app.get('/',(req,res)=>{
+    app.get('*',(req,res)=>{
       res.sendFile(path.resolve(__dirname,'react','build','index.html'))
     })
   };

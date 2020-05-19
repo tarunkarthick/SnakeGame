a94a21a8162493b8b3154ed51ed44a031e22a04d
tarunkarthick <tarunkarthick.7@gmail.com>
2020-05-19 13:05:50 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express=require('express')
+
+const app=express()
+
+const cors=require('cors')
+const path=require('path')
+const dotenv=require('dotenv')
+dotenv.config()
+
+app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('react/build')); // serve the static react app
+    app.get('/',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'react','build','index.html'))
+    })
+  };
+
+
+  const port=process.env.PORT||3001
+
+  app.listen(port,()=>{
+      console.log(`A node js API is listening on port:${port}`)
+  })
